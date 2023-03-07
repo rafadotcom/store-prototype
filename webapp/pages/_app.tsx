951b5250be78e49a1ea609190b03568c773d6b21
Rafael <fc56891@alunos.fc.ul.pt>
2023-03-07 16:34:57 +0000

    Tentativa de merge com o branch sonia

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
+import customTheme from "../styles/theme";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={customTheme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
+
---
+++ webapp/pages/login.tsx
@@ -0,0 +1,75 @@
+import { Box, Button, FormControl, FormLabel, Heading, Input, Link, Text } from "@chakra-ui/react";
+import { ThemeProvider } from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import theme from "../styles/styles";
+import { FormEvent, useState } from "react";
+import Navbar from "../components/Navbar"; // importe o componente Navbar
+
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          body {
+            background-image: url('/cuca.jpg');
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
+            overflow-x: hidden;
+          }
+        `}
+      />
+      <Navbar />
+      <Box h="100vh" display="flex" alignItems="center" justifyContent="center">
+        <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+          <Box bg="#8B4000" p="4">
+            <Heading size="md" color="white">
+              Faça o login na sua conta
+            </Heading>
+          </Box>
+          <Box p="4">
+            <form onSubmit={handleSubmit}>
+              <FormControl id="email" mb="4">
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type="email"
+                  placeholder="Seu email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormControl>
+              <FormControl id="password" mb="4">
+                <FormLabel>Senha</FormLabel>
+                <Input
+                  type="password"
+                  placeholder="Sua senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </FormControl>
+              <Button type="submit" width="full">
+                Entrar
+              </Button>
+            </form>
+            <Text mt="4" textAlign="center">
+              Não tem uma conta ainda?{" "}
+              <Link color="#8B4000" href="#">
+                Registre-se aqui
+              </Link>
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
