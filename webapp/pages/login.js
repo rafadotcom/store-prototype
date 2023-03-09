0183e0a524a82763f3deb9f26cd544152caf9b23
sonia <fc56898@alunos.fc.ul.pt>
2023-03-09 20:31:18 +0000

    profile

--- webapp/pages/login.js
+++ webapp/pages/login.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@chakra-ui/react";
 import { Global, css } from "@emotion/react";
 import theme from "../styles/styles";
 import { useState } from "react";
-import Navbar from "../components/Navbar"; // importe o componente Navbar
+import Navbar from "../components/Navbar_sign"; // importe o componente Navbar
 
 
 export default function Login() {
@@ -21,7 +21,7 @@ export default function Login() {
       <Global
         styles={css`
           body {
-            background-image: url('/cuca.jpg');
+            background-image: url('/fundo.jpg');
             background-repeat: no-repeat;
             background-position: center;
             background-size: cover;
@@ -29,10 +29,16 @@ export default function Login() {
           }
         `}
       />
+      <Box
+        background="rgb(1,1,1,0.6)"
+        width="100%"
+        height="100%"
+        zIndex="100"
+        >
       <Navbar />
       <Box h="100vh" d="flex" alignItems="center" justifyContent="center">
         <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-          <Box bg="#8B4000" p="4">
+          <Box bg="#65000b" p="4">
             <Heading size="md" color="white">
               Faça o login na sua conta
             </Heading>
@@ -63,12 +69,12 @@ export default function Login() {
             </form>
             <Text mt="4" textAlign="center">
               Não tem uma conta ainda?{" "}
-              <Link color="#8B4000" href="#">
+              <Link color="#65000b" href="#">
                 Registre-se aqui
               </Link>
             </Text>
           </Box>
-        </Box>
+        </Box></Box>
       </Box>
     </ThemeProvider>
   );
