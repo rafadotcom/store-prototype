c3da899aa408db668368a7b15beaccdab45d6418
Gonçalo Nogueira <gn16102000@gmail.com>
2023-05-24 23:40:50 +0100

    gonçalo2

--- webapp/pages/bolos.tsx
+++ webapp/pages/bolos.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Flex, FormLabel, Heading, Image, Input, InputGroup, Text, ThemeProvider } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import { Box, Heading, Text, Flex, Image, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { ThemeProvider, InputGroup } from "@chakra-ui/react";
 import theme from "../styles/styles";
+import Navbar from "../components/Navbar";
 
 const initialProductState = {
   id: "",
@@ -16,7 +17,7 @@ export default function Produtos() {
 
 
   useEffect(() => {
-    fetch("api/getBolos", {
+    fetch("https://webstore-backend-nu.vercel.app/api/getBolos", {
       method: "GET"
     })
       .then((res) => res.json())
@@ -29,20 +30,11 @@ export default function Produtos() {
   const [products, setProducts] = useState([]);
   const [newProduct, setNewProduct] = useState(initialProductState);
   const [showAddProduct, setShowAddProduct] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
 
   const handleCancelAddProduct = () => {
     setNewProduct(initialProductState);
     setShowAddProduct(false);
   };
-
-  const handleAddToCart = (product) => {
-    console.log("Product added to cart:", product);
-  };
-
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <ThemeProvider theme={theme} >
@@ -63,117 +55,117 @@ export default function Produtos() {
           {
             showAddProduct ? (
               <Box p={6} >
-                <form action="/api/addBolos" method="post" >
+                <form action="https://webstore-backend-nu.vercel.app/api/addBolo" method="post" >
                   <InputGroup mb="4" >
-                    <FormLabel color="white" > Nome do produto < /FormLabel>
-                      < Input
-                        type="nome do Produto"
-                        placeholder="Nome do produto"
-                        name="name"
-                        color="white"
-                      />
+                    <FormLabel color="white" > Nome do produto </FormLabel>
+                    < Input
+                      type="nome do Produto"
+                      placeholder="Nome do produto"
+                      name="name"
+                      color="white"
+                    />
                   </InputGroup>
 
                   < InputGroup mb="4" >
-                    <FormLabel color="white" > Descrição < /FormLabel>
-                      < Input
-                        type="descricao"
-                        placeholder="Descrição do Produto"
-                        name="description"
-                        color="white"
-                      />
+                    <FormLabel color="white" > Descrição </FormLabel>
+                    < Input
+                      type="descricao"
+                      placeholder="Descrição do Produto"
+                      name="description"
+                      color="white"
+                    />
                   </InputGroup>
 
                   < InputGroup mb="4" >
-                    <FormLabel color="white" > Preço < /FormLabel>
-                      < Input
-                        type="preco"
-                        placeholder="Preço do produto"
-                        name="price"
-                        color="white"
-                      />
+                    <FormLabel color="white" > Preço </FormLabel>
+                    < Input
+                      type="preco"
+                      placeholder="Preço do produto"
+                      name="price"
+                      color="white"
+                    />
                   </InputGroup>
 
 
                   < Flex justify="space-between" >
-                    <Button onClick={handleCancelAddProduct}> Cancelar < /Button>
-                      < Button
-                        type="submit"
-                        colorScheme="green"
-                        disabled={!newProduct.name || !newProduct.description || !newProduct.price
-                        }>
-                        Adicionar Produto
-                        < /Button>
+                    <Button onClick={handleCancelAddProduct}> Cancelar </Button>
+                    < Button
+                      type="submit"
+                      colorScheme="green"
+                      disabled={!newProduct.name || !newProduct.description || !newProduct.price
+                      }>
+                      Adicionar Produto
+                    </Button>
 
+                    < Button
+                      /*action = {"api/getBolos.js"}*/
+                      colorScheme="green"
+                      disabled={!newProduct.name || !newProduct.description || !newProduct.price
+                      }>
+                      Adicionar Produto
+                    </Button>
+                  </Flex>
+                </form>
+              </Box>
+            ) : (
+              <>
+                <Box p={6} >
+                  <Heading as="h1" size="xl" textAlign="center" mb={6} >
+                    Produtos
+                  </Heading>
+                  < Button
+                    bg="#deb887"
+                    onClick={() => setShowAddProduct(true)}
+                    mb={6}>
+                    Adicionar Produto
+                  </Button>
+                </Box>
+
+                < Flex flexWrap="wrap" p={6} >
+                  {products.map((product) => (
+                    <Box
+                      key={product.id}
+                      p={3}
+                      mb={4}
+                      mr={4}
+                      width={{ base: "40%", md: "48%", lg: "32%" }}
+                      borderWidth="1px"
+                      borderRadius="lg"
+                      overflow="hidden"
+                      bg="#faf0e6">
+                      <Image src={"/bolo1.png"}
+                        alt={product.name}
+                        width="100%"
+                        height="auto"
+                        objectFit="cover"
+                        borderRadius="lg"
+                      />
+
+                      <Box mt="1" fontWeight="bold" fontSize="20px" as="h4" lineHeight="tight" isTruncated >
+                        {product.name}
+                      </Box>
+
+                      < Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated >
+                        {product.description}
+                      </Box>
+
+                      < Box display="flex" mt="2" alignItems="center" >
+                        <Text fontWeight="semibold" fontSize="30px" color="black"> {product.price} </Text>
                         < Button
-                          action={"api/getBolos.js"}
-                          colorScheme="green"
-                          disabled={!newProduct.name || !newProduct.description || !newProduct.price
-                          }>
-                          Adicionar Produto
-                          < /Button>
-                          < /Flex>
-                          < /form>
-                          < /Box>
-                          ) : (
-    <>
-                            <Box p={6} >
-                              <Heading as="h1" size="xl" textAlign="center" mb={6} >
-                                Produtos
-                                < /Heading>
-                                < Button
-                                  bg="#deb887"
-                                  onClick={() => setShowAddProduct(true)}
-                                  mb={6}>
-                                  Adicionar Produto
-                                  < /Button>
-                                  < /Box>
-
-                                  < Flex flexWrap="wrap" p={6} >
-                                    {products.map((product) => (
-                                      <Box
-                                        key={product.id}
-                                        p={3}
-                                        mb={4}
-                                        mr={4}
-                                        width={{ base: "40%", md: "48%", lg: "32%" }}
-                                        borderWidth="1px"
-                                        borderRadius="lg"
-                                        overflow="hidden"
-                                        bg="#faf0e6">
-                                        <Image src={"/bolo1.png"}
-                                          alt={product.name}
-                                          width="100%"
-                                          height="auto"
-                                          objectFit="cover"
-                                          borderRadius="lg"
-                                        />
-
-                                        <Box mt="1" fontWeight="bold" fontSize="20px" as="h4" lineHeight="tight" isTruncated >
-                                          {product.name}
-                                          < /Box>
-
-                                          < Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated >
-                                            {product.description}
-                                            < /Box>
-
-                                            < Box d="flex" mt="2" alignItems="center" >
-                                              <Text fontWeight="semibold" fontSize="30px" color="black"> {product.price} < /Text>
-                                                < Button
-                                                  ml="auto"
-                                                  bg="#deb887"
-                                                  onClick={() => handleAddToCart(product)}
-                                                >
-                                                  Adicionar ao carrinho
-                                                  < /Button>
-                                                  < /Box>
-                                                  < /Box>
-    ))}
-                                                </Flex>
-                                                < />
-)}
-                                            </Box>
-                                            < /Box>
-                                            < /ThemeProvider>
-                                            );
+                          ml="auto"
+                          bg="#deb887"
+                        /*onClick = {() => handleAddToCart(product)}*/
+                        >
+                          Adicionar ao carrinho
+                        </Button>
+                      </Box>
+                    </Box>
+                  ))}
+                </Flex>
+              </>
+            )}
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
