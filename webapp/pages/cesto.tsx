6bfd3b871e086eaf577579f02ba4431f7d42977b
Rafael <fc56891@alunos.fc.ul.pt>
2023-07-11 20:17:09 +0100

    Melhorias

--- webapp/pages/cesto.tsx
+++ webapp/pages/cesto.tsx
@@ -271,7 +271,7 @@ export default function Cesto() {
                     //produto nao encontrado
                     item.found ? (
                       //se o produto for encontrado numa das listas  
-                      <GridItem bg="#faf0e6" borderRadius="10px" key={item._id}>
+                      <GridItem bg="#faf0e6" borderRadius="10px" key={item._id} minWidth={0}>
                         <Box mt="1" margin="1rem" fontWeight="bold" fontSize="20px" as="h4" lineHeight="tight" isTruncated >
                         {item.name}
                         </Box>
@@ -282,12 +282,12 @@ export default function Cesto() {
                           objectFit="cover"
                           borderRadius="lg"
                         />
-                        <Box display="flex" alignItems="center" justifyContent="space-between">
+                        <Box display="flex" alignItems="center" justifyContent="space-between" minWidth="fit-content">
                           <Box ml="1.5rem" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
                             {parseFloat(item.price)*item.quantidade} €
                           </Box>
-                          <NumberInput size="sm" defaultValue={item.quantidade} margin="1rem" focusBorderColor="#deb887" maxW={32} min={0} precision={0} onChange={(value) => handleQuantityChange(item._id, value)}>
-                            <NumberInputField bg="white" border="1px solid #deb887"/>
+                          <NumberInput minWidth={0} size="sm" defaultValue={item.quantidade} margin="1rem" focusBorderColor="#deb887" maxW={24} min={0} precision={0} onChange={(value) => handleQuantityChange(item._id, value)}>
+                            <NumberInputField minWidth="fit-content" borderRadius="md" bg="white" border="1px solid #deb887"/>
                             <NumberInputStepper><NumberIncrementStepper /><NumberDecrementStepper /></NumberInputStepper>
                           </NumberInput>
                         </Box>
